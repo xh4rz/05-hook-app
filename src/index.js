24731b9322b8386b78b1ca0d75347743220a0900
xh4rz <xh4rz@hotmail.com>
2021-05-25 11:25:04 -0500

    14- useLayoutEffect

--- src/index.js
+++ src/index.js
@@ -8,7 +8,8 @@ import ReactDOM from 'react-dom';
 // import {FormWithCustomHook} from './components/02-useEffect/FormWithCustomHook'
 // import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
 // import { FocusScreen } from './components/04-useRef/FocusScreen';
-import { RealExampleRef } from './components/04-useRef/RealExampleRef';
+// import { RealExampleRef } from './components/04-useRef/RealExampleRef';
+import { Layout } from './components/05-useLayoutEffect/Layout';
 
 
-ReactDOM.render(<RealExampleRef />, document.getElementById('root'));
+ReactDOM.render(<Layout />, document.getElementById('root'));

ba518ef9e08dbccbb31152a42dfeaa1ee2caf48d
xh4rz <xh4rz@hotmail.com>
2021-05-25 01:40:00 -0500

    13- UseRef - Caso de uso real

---
+++ src/components/01-useState/CounterApp.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import './counter.css';
+
+export const CounterApp = () => {
+	const [state, setState] = useState({
+		counter1: 10,
+		counter2: 20,
+		counter3: 30,
+		counter4: 40
+	});
+
+	// console.log(counter);
+
+	const { counter1, counter2 } = state;
+
+	return (
+		<>
+			<h1>Counter 1 {counter1}</h1>
+			<h1>Counter 2 {counter2}</h1>
+
+			<hr />
+			<button
+				className="btn btn-primary"
+				onClick={() => {
+					setState({
+						...state,
+						counter1: counter1 + 1
+					});
+				}}
+			>
+				+1
+			</button>
+		</>
+	);
+};
---
+++ src/components/04-useRef/FocusScreen.js
@@ -0,0 +1,23 @@
+import React, { useRef } from 'react';
+import '../02-useEffect/effects.css';
+
+export const FocusScreen = () => {
+	const inputRef = useRef();
+	// console.log(ref);
+
+	const handleClick = () => {
+		inputRef.current.select();
+        console.log(inputRef);
+	};
+
+	return (
+		<div>
+			<h1>Focus Screen</h1>
+			<hr />
+			<input className="form-control" placeholder="Su nombre" ref={inputRef} />
+			<button className="btn btn-outline-primary mt-5" onClick={handleClick}>
+				Focus
+			</button>
+		</div>
+	);
+};

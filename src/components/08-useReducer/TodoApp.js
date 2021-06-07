638fdbf5ca7cddcaa1106eab8f30f8d9fc38233c
xh4rz <xh4rz@hotmail.com>
2021-06-07 01:43:52 -0500

    Optimización #1 - Listado de TODOS

--- src/components/08-useReducer/TodoApp.js
+++ src/components/08-useReducer/TodoApp.js
@@ -2,6 +2,7 @@ import React, { useEffect, useReducer } from 'react';
 import { useForm } from '../../hooks/useForm';
 import { todoReducer } from './todoReducer';
 import './styles.css';
+import { TodoList } from './TodoList';
 
 const init = () => {
 	return JSON.parse(localStorage.getItem('todos')) || [];
@@ -27,7 +28,6 @@ export const TodoApp = () => {
 	}, [todos]);
 
 	const handleDelete = (todoId) => {
-
 		const action = {
 			type: 'delete',
 			payload: todoId
@@ -36,14 +36,12 @@ export const TodoApp = () => {
 		dispatch(action);
 	};
 
-    const handleToggle = ( todoId ) => {
-        dispatch ({
-            type: 'toggle',
-            payload: todoId
-        })
-    }
-
-
+	const handleToggle = (todoId) => {
+		dispatch({
+			type: 'toggle',
+			payload: todoId
+		});
+	};
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
@@ -73,25 +71,13 @@ export const TodoApp = () => {
 			<hr />
 			<div className="row">
 				<div className="col-7">
-					<ul className="list-group list-group-flush">
-						{todos.map((todo, i) => (
-							<li key={todo.id} className="list-group-item">
-								<p 
-                                className={`${todo.done && 'complete'}`}
-                                onClick={() => handleToggle(todo.id)}
-                                >
-									{i + 1}. {todo.desc}
-								</p>
-								<button
-									className="btn btn-danger"
-									onClick={() => handleDelete(todo.id)}
-								>
-									Borrar
-								</button>
-							</li>
-						))}
-					</ul>
+					<TodoList
+						todos={todos}
+						handleDelete={handleDelete}
+						handleToggle={handleToggle}
+					/>
 				</div>
+
 				<div className="col-5">
 					<h4>Agregar TODO</h4>
 					<hr />

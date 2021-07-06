413e4a3922332d52b6e54b890628b76db9e6dc57
Harold Gonzalez <xh4rz@hotmail.com>
2021-07-05 23:23:36 -0500

    7._Pruebas_sobre_useFetch_-_CustomHook

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -28,6 +28,14 @@ export const useFetch = (url) => {
 						data
 					});
 				}
+			})
+
+			.catch(() => {
+				setState({
+					data: null,
+					loading: false,
+					error: 'No se pudo cargar la info'
+				});
 			});
 	}, [url]);
 

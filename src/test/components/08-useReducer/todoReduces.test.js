64fa57e4cd4f22a27c44a3525edfb26ad29c30a5
Harold Gonzalez <xh4rz@hotmail.com>
2021-07-27 23:23:27 -0500

    11._Pruebas_restantes_de_mi_Reducer

--- src/test/components/08-useReducer/todoReduces.test.js
+++ src/test/components/08-useReducer/todoReduces.test.js
@@ -24,4 +24,26 @@ describe('Pruebas en todoReducer', () => {
 		expect(state.length).toBe(3);
 		expect(state).toEqual([...demoTodos, newTodo]);
 	});
+
+	test('debe de borrar un TODO', () => {
+		const action = {
+			type: 'delete',
+			payload: 1
+		};
+
+		const state = todoReducer(demoTodos, action);
+		expect(state.length).toBe(1);
+		expect(state).toEqual([demoTodos[1]]);
+	});
+
+	test('debe de hacer el TOGGLE DEL TODO', () => {
+		const action = {
+			type: 'toggle',
+			payload: 1
+		};
+
+		const state = todoReducer(demoTodos, action);
+		expect(state[0].done).toBe(true);
+		expect(state[1]).toEqual(demoTodos[1]);
+	});
 });
